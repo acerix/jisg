8e3ee7dbcc41cea9d0cf228c26c43d7cb9733b10
acerix <dylan@psilly.com>
2021-01-31 16:19:42 -0500

    Self-ref Kolakoski

--- example.js
+++ example.js
@@ -5,6 +5,24 @@ const JISG = require('./dist/index').default
 
 const jisg = new JISG()
 
-for (const n of jisg.A000040()) {
+/*
+for (const n of jisg.A000045()) {
+    console.log(n)
+}
+*/
+
+function slice_generator(generator, start, end) {
+    let i = 0
+    const r = []
+    while (i++<start) {
+        generator.next()
+    }
+    while (i++<=end) {
+        r.push(generator.next().value)
+    }
+    return r
+}
+
+for (const n of slice_generator(jisg.A000002(),0,20)) {
     console.log(n)
 }
