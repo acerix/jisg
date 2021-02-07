b1e5a53b34711097152cdf73910922842a273eb4
acerix <dylan@psilly.com>
2021-02-06 23:52:47 -0500

    Refec

--- src/oeis/A000668.js
+++ src/oeis/A000668.js
@@ -1,14 +1,11 @@
 // Mersenne primes
 import is_prime from 'is-prime'
+import A000040 from './A000040'
 export default function* A000668() {
-    let n = 2
-    while (true) {
-        if (is_prime(n)) {
-            const x = Math.pow(2, n) - 1
-            if (is_prime(x)) {
-                yield x
-            }
+    for (const n of A000040()) {
+        const x = Math.pow(2, n) - 1
+        if (is_prime(x)) {
+            yield x
         }
-        n++
     }
 }
