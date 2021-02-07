1e46cce5140259c83a667f841065b37fb48ae3ec
acerix <dylan@psilly.com>
2021-02-06 23:47:59 -0500

    optimize to use internal prime generator

--- src/oeis/A000043.js
+++ src/oeis/A000043.js
@@ -1,11 +1,10 @@
 // Mersenne exponents
 import is_prime from 'is-prime'
+import A000040 from './A000040'
 export default function* A000043() {
-    let n = 2
-    while (true) {
-        if (is_prime(n) && is_prime(Math.pow(2, n) - 1)) {
+    for (const n of A000040()) {
+        if (is_prime(Math.pow(2, n) - 1)) {
             yield n
         }
-        n++
     }
 }
