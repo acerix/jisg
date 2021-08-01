ae1e5855def8e2ee030d5e65b4666ff0076491b1
acerix <dylan@psilly.com>
2021-07-31 23:25:38 -0400

    Fix type

--- src/oeis/A059252.js
+++ src/oeis/A059252.js
@@ -3,7 +3,7 @@ export default function* A059252() {
     for (let t=0; true; t++) {
         const n = Math.ceil(Math.log2(t+1)/2)
         const s = [(n&1)===1, false]
-        const y = 0
+        let y = 0
         for (let i=n-1; i!==-1; i--) {
             const x = 1<<(i<<1)
             const a = (t & (x<<1)) !== 0
