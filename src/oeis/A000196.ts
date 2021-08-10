cce9df2ce7a23c56a910a332ef507d7ac9b57e26
acerix <dylan@psilly.com>
2021-08-09 22:55:33 -0400

    Realize 196

--- src/oeis/A000196.ts
+++ src/oeis/A000196.ts
@@ -1,6 +1,6 @@
 // Integer part of square root of n. Or, number of positive squares <= n. Or, n appears 2n+1 times.
 export default function* A000196(): Generator<bigint> {
-  for (const n of [0n, 1n, 1n, 1n, 2n, 2n, 2n, 2n, 2n, 3n, 3n, 3n, 3n, 3n, 3n, 3n, 4n, 4n, 4n, 4n, 4n, 4n, 4n, 4n, 4n, 5n, 5n, 5n, 5n, 5n, 5n, 5n, 5n, 5n, 5n, 5n, 6n, 6n, 6n, 6n, 6n, 6n, 6n, 6n, 6n, 6n, 6n, 6n, 6n, 7n, 7n, 7n, 7n, 7n, 7n, 7n, 7n, 7n, 7n, 7n, 7n, 7n, 7n, 7n, 8n, 8n, 8n, 8n, 8n, 8n, 8n, 8n, 8n, 8n, 8n, 8n, 8n, 8n, 8n, 8n, 8n, 9n, 9n, 9n, 9n, 9n, 9n, 9n, 9n, 9n, 9n, 9n, 9n, 9n, 9n, 9n, 9n, 9n, 9n, 9n, 10n, 10n, ]) {
-    yield n
+  for (let n = 0n; ; n++) {
+    yield BigInt(Math.sqrt(Number(n)) | 0)
   }
 }
