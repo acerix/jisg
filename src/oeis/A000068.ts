e207533ef360771a02cd06f473bfd442c7f8d10b
acerix <dylan@psilly.com>
2021-08-09 22:38:57 -0400

    Realize 68

--- src/oeis/A000068.ts
+++ src/oeis/A000068.ts
@@ -1,6 +1,8 @@
+import { isPrime } from "../utils";
+
 // Numbers n such that n^4 + 1 is prime.
 export default function* A000068(): Generator<bigint> {
-  for (const n of [1n, 2n, 4n, 6n, 16n, 20n, 24n, 28n, 34n, 46n, 48n, 54n, 56n, 74n, 80n, 82n, 88n, 90n, 106n, 118n, 132n, 140n, 142n, 154n, 160n, 164n, 174n, 180n, 194n, 198n, 204n, 210n, 220n, 228n, 238n, 242n, 248n, 254n, 266n, 272n, 276n, 278n, 288n, 296n, 312n, 320n, 328n, 334n, 340n, 352n, 364n, 374n, 414n, 430n, 436n, 442n, 466n, ]) {
-    yield n
+  for (let n = 1n; ; n++) {
+    if (isPrime(n**4n+1n)) yield n
   }
 }
