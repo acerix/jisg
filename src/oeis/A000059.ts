c5d8b44e8a6a9d427739f260653fcd9dd3fd7d7b
acerix <dylan@psilly.com>
2021-08-10 03:33:09 -0400

    Similarize

--- src/oeis/A000059.ts
+++ src/oeis/A000059.ts
@@ -1,11 +1,8 @@
+import { isPrime } from '../utils'
+
 // Numbers n such that (2n)^4 + 1 is prime.
-import {isPrime} from '../utils'
 export default function* A000059(): Generator<bigint> {
-  let n = 1n
-  for (;;) {
-    if (isPrime((2n * n)**4n + 1n)) {
-      yield n
-    }
-    n++
+  for (let n = 1n; ; n++) {
+    if (isPrime((2n * n)**4n + 1n)) yield n
   }
 }
