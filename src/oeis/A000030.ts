aad6139c74f0e7b225e8e1995fd78c4633519098
acerix <dylan@psilly.com>
2021-11-07 21:30:09 -0500

    implement A000030, ezee

--- src/oeis/A000030.ts
+++ src/oeis/A000030.ts
@@ -1,6 +1,6 @@
 // Initial digit of n.
 export default function* A000030(): Generator<bigint> {
-  for (const n of [0n, 1n, 2n, 3n, 4n, 5n, 6n, 7n, 8n, 9n, 1n, 1n, 1n, 1n, 1n, 1n, 1n, 1n, 1n, 1n, 2n, 2n, 2n, 2n, 2n, 2n, 2n, 2n, 2n, 2n, 3n, 3n, 3n, 3n, 3n, 3n, 3n, 3n, 3n, 3n, 4n, 4n, 4n, 4n, 4n, 4n, 4n, 4n, 4n, 4n, 5n, 5n, 5n, 5n, 5n, 5n, 5n, 5n, 5n, 5n, 6n, 6n, 6n, 6n, 6n, 6n, 6n, 6n, 6n, 6n, 7n, 7n, 7n, 7n, 7n, 7n, 7n, 7n, 7n, 7n, 8n, 8n, 8n, 8n, 8n, ]) {
-    yield n
+  for (let n=0n;;n++) {
+    yield BigInt(n.toString()[0])
   }
 }
