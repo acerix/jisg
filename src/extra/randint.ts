c0818f376b254b99d7ab017bd53b40cedbf3a379
acerix <dylan@psilly.com>
2021-11-15 23:34:25 -0500

    it was good, for a while

--- src/extra/randint.ts
+++ src/extra/randint.ts
@@ -1,8 +1,8 @@
-// Random integer
+// Random integer generator
 export default function* randint(min?: number, max?: number): Generator<number> {
   min = Math.ceil(min || 0)
   max = Math.floor(max || Number.MAX_SAFE_INTEGER)
-  while (true) {
+  for (;;) {
     yield Math.floor(Math.random() * (max - min + 1)) + min
   }
 }
