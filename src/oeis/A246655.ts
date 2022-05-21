f8b81e7991d1833b785a54681837f8ca6d89dec0
Dylan Ferris <dylan@psilly.com>
2022-05-20 20:00:39 -0400

    do A246655

---
+++ src/oeis/A246655.ts
@@ -0,0 +1,34 @@
+import A000040 from './A000040'
+
+// Prime powers: numbers of the form p^k where p is a prime and k >= 1.
+export default function* A246655(): Generator<bigint> {
+  const primes: bigint[] = []
+  const primesCurrentExponent: bigint[] = []
+  const r = new Set<bigint>()
+  for (const n of A000040()) {
+    for (const v of [...r].sort((a, b) => Number(a - b))) {
+      if (v > n) {
+        break
+      }
+      yield v
+      r.delete(v)
+    }
+    yield n
+    primes.push(n)
+    primesCurrentExponent.push(2n)
+    Object.keys(primes).forEach((i) => {
+      const j = parseInt(i, 10)
+      let value = -1n
+      console.log('j', j, primesCurrentExponent[j])
+      for (
+        let exponent = primesCurrentExponent[j] as bigint;
+        value < n;
+        exponent++
+      ) {
+        value = (primes[j] as bigint) ** exponent
+        r.add(value)
+        primesCurrentExponent[j]++
+      }
+    })
+  }
+}
