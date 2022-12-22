67228e4d0f05928af510b86942f1c79c151f75d5
Dylan Ferris <dylan@psilly.com>
2022-12-22 02:16:13 -0500

    betterize

--- src/oeis/A000002.ts
+++ src/oeis/A000002.ts
@@ -1,12 +1,10 @@
 // Kolakoski sequence
 export default function* A000002(): Generator<bigint> {
-  for (const n of [1n, 2n, 2n]) {
-    yield n
-  }
-  let p = true
+  yield 1n, 2n, 2n
   const g = A000002()
   g.next()
   g.next()
+  let p = true
   for (const r of g) {
     p = !p
     for (let i = 0n; i < r; i++) {
