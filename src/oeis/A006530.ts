e7c1e5bbeed63411f1bc8b8637ac0c0e086b02bd
Dylan Ferris <d@psilly.com>
2023-08-31 23:18:55 -0400

    less is more

--- src/oeis/A006530.ts
+++ src/oeis/A006530.ts
@@ -5,7 +5,6 @@ export default function* A006530(): Generator<bigint> {
   yield 1n
   for (let n = 2n; n; n++) {
     const pf = primeFactorization(n)
-    const p = pf.length - 1
-    yield pf[p] as bigint
+    yield pf[pf.length - 1] as bigint
   }
 }
