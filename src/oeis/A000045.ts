40fa36333a43ca41b63b3f84f975e5b6f81cdfe0
Dylan Ferris <d@psilly.com>
2023-09-02 21:30:09 -0400

    Implement Tribonacci, Tetranacci

--- src/oeis/A000045.ts
+++ src/oeis/A000045.ts
@@ -1,13 +1,12 @@
-// Fibonacci numbers
+// Fibonacci numbers: a(n) = a(n-1) + a(n-2) for n >= 2 with a(0) = 0 and a(1) = 1.
 export default function* A000045(): Generator<bigint> {
-  let previous = 0n,
-    current = 1n
-  yield previous
-  yield current
+  let a0 = 0n, a1 = 1n
+  yield a0
+  yield a1
   for (;;) {
-    const sum = current + previous
-    previous = current
-    current = sum
-    yield current
+    const sum = a0 + a1
+    a0 = a1
+    a1 = sum
+    yield a1
   }
 }
