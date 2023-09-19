67e6e64af9f7f70ed93e6958302d811df76c797b
Dylan Ferris <d@psilly.com>
2023-09-19 02:06:32 -0400

    biggify A008284

--- src/oeis/A008284.ts
+++ src/oeis/A008284.ts
@@ -1,15 +1,15 @@
 // Triangle of partition numbers: T(n,k) = number of partitions of n in which the greatest part is k, 1 <= k <= n. Also number of partitions of n into k positive parts, 1 <= k <= n.
 
-function T(n: number, k: number): number {
-  if (k < 1 || k > n) return 0
-  if (k === 1 || k === n) return 1
-  return T(n-1, k-1) + T(n-k, k)
+function T(n: bigint, k: bigint): bigint {
+  if (k < 1n || k > n) return 0n
+  if (k === 1n || k === n) return 1n
+  return T(n-1n, k-1n) + T(n-k, k)
 }
 
 export default function* A008284(): Generator<bigint> {
-  for (let n=1; /"*∞*"/; n++) {
-    for (let k=1; k<=n; k++) {
-      yield BigInt(T(n, k))
+  for (let n=1n; /"*∞*"/; n++) {
+    for (let k=1n; k<=n; k++) {
+      yield T(n, k)
     }
   }
 }
