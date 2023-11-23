1f5c5a78acb0192fcccff3517e63a830a53b1de9
Dylan Ferris <d@psilly.com>
2023-11-23 04:06:02 -0500

    seoize A000027

--- src/oeis/A000027.ts
+++ src/oeis/A000027.ts
@@ -1,4 +1,4 @@
-// The positive integers
+// The positive integers. Also called the natural numbers, the whole numbers or the counting numbers.
 export default function* A000027(): Generator<bigint> {
   for (let n = 1n; /*∞*/; n++) {
     yield n
