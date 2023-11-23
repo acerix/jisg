01d09828e850ba78eba0557b382c50b07fd2fc2c
Dylan Ferris <d@psilly.com>
2023-11-23 04:42:22 -0500

    Add A235921

--- src/oeis/A235921.ts
+++ src/oeis/A235921.ts
@@ -1,7 +1,13 @@
+import A236454 from './A236454'
+import A053669 from './A053669'
+
 // Numbers n such that smallest number not dividing n^2 (A236454) is different from smallest prime not dividing n (A053669). 
-const f = 210n
-export default function* A236432(): Generator<bigint> {
-  for (let n = f; /*∞*/; n += 2n*f) {
-    yield n
+export default function* A235921(): Generator<bigint> {
+  const g = A236454()
+  const h = A053669()
+  for (let n = 0n; /*∞*/; n++) {
+    if (g.next().value !== h.next().value) {
+      yield n
+    }
   }
 }
