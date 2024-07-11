4886e0b7dfa7ae4f2e30b939cf6a8a9ca52e97fa
Dylan Ferris <d@psilly.com>
2024-07-10 23:45:38 -0400

    implement A000245

--- src/oeis/A000108.ts
+++ src/oeis/A000108.ts
@@ -4,6 +4,5 @@ import { factorial } from '../utils'
 export default function* A000108(): Generator<bigint> {
   for (let n = 0n; /*∞*/; n++) {
     yield factorial(2n * n) / factorial(n + 1n) / factorial(n)
-    
   }
 }
