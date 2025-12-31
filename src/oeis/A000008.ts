99c8387514b2ca024520d3948b264c333468f83c
Dylan Ferris <psient@proton.me>
2025-12-31 18:13:41 -0500

    yolo A000008

--- src/oeis/A000008.ts
+++ src/oeis/A000008.ts
@@ -1,68 +1,19 @@
 // Number of ways of making change for n cents using coins of 1, 2, 5, and 10 cents
 export default function* A000008(): Generator<bigint> {
-  for (const n of [
-    1n,
-    1n,
-    2n,
-    2n,
-    3n,
-    4n,
-    5n,
-    6n,
-    7n,
-    8n,
-    11n,
-    12n,
-    15n,
-    16n,
-    19n,
-    22n,
-    25n,
-    28n,
-    31n,
-    34n,
-    40n,
-    43n,
-    49n,
-    52n,
-    58n,
-    64n,
-    70n,
-    76n,
-    82n,
-    88n,
-    98n,
-    104n,
-    114n,
-    120n,
-    130n,
-    140n,
-    150n,
-    160n,
-    170n,
-    180n,
-    195n,
-    205n,
-    220n,
-    230n,
-    245n,
-    260n,
-    275n,
-    290n,
-    305n,
-    320n,
-    341n,
-    356n,
-    377n,
-    392n,
-    413n,
-    434n,
-    455n,
-    476n,
-    497n,
-    518n,
-    546n,
-  ]) {
-    yield n
+  const f2: bigint[] = []
+  const f3: bigint[] = []
+  const f4: bigint[] = []
+
+  for (let n = 0; ; n++) {
+    const valF2 = 1n + (n >= 2 ? f2[n - 2] : 0n)
+    f2.push(valF2)
+
+    const valF3 = valF2 + (n >= 5 ? f3[n - 5] : 0n)
+    f3.push(valF3)
+
+    const valF4 = valF3 + (n >= 10 ? f4[n - 10] : 0n)
+    f4.push(valF4)
+
+    yield valF4
   }
 }
