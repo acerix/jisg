b03aec60e80efe0df6b1ace1f393284bc4ce7c7a
Dylan Ferris <psient@proton.me>
2025-12-31 19:28:03 -0500

    feat: add A000666 sequence (Number of symmetric relations on n nodes)

--- src/oeis/A000666.ts
+++ src/oeis/A000666.ts
@@ -1,25 +1,55 @@
-// Number of symmetric relations on n nodes
+import { factorial, gcd } from '../utils'
+
+function* partitions(n: number, min: number = 1): Generator<number[]> {
+  if (n === 0) {
+    yield []
+    return
+  }
+  for (let i = min; i <= n; i++) {
+    for (const p of partitions(n - i, i)) {
+      yield [i, ...p]
+    }
+  }
+}
+
+function permcount(v: number[], n: bigint): bigint {
+  let m = 1n
+  let k = 0n
+  for (let i = 0; i < v.length; i++) {
+    const t = BigInt(v[i])
+    if (i > 0 && v[i] === v[i - 1]) {
+      k += 1n
+    } else {
+      k = 1n
+    }
+    m *= t * k
+  }
+  return factorial(n) / m
+}
+
+function edges(v: number[]): bigint {
+  let sum1 = 0n
+  for (let i = 1; i < v.length; i++) {
+    for (let j = 0; j < i; j++) {
+      sum1 += gcd(BigInt(v[i]), BigInt(v[j]))
+    }
+  }
+  
+  let sum2 = 0n
+  for (let i = 0; i < v.length; i++) {
+    sum2 += BigInt(Math.floor(v[i] / 2) + 1)
+  }
+  
+  return sum1 + sum2
+}
+
 export default function* A000666(): Generator<bigint> {
-  for (const n of [
-    1n,
-    2n,
-    6n,
-    20n,
-    90n,
-    544n,
-    5096n,
-    79264n,
-    2208612n,
-    113743760n,
-    10926227136n,
-    1956363435360n,
-    652335084592096n,
-    405402273420996800n,
-    470568642161119963904n,
-    1023063423471189431054720n,
-    4178849203082023236058229792n,
-    32168008290073542372004082199424n,
-  ]) {
-    yield n
+  for (let n = 0; ; n++) {
+    const nBig = BigInt(n)
+    let s = 0n
+    for (const p of partitions(n)) {
+      s += permcount(p, nBig) * (2n ** edges(p))
+    }
+    yield s / factorial(nBig)
   }
 }
